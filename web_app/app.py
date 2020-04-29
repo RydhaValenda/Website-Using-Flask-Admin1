1bc3d69f43ec3719a3dd3e9db6bcf1a4378c75e6
RydhaValenda <ryval.study@gmail.com>
2020-04-29 12:33:57 +0700

    Update - 04 - create  model object has a relationship from menu to page and vice versa - can call the contents of cms dynamically

--- web_app/app.py
+++ web_app/app.py
@@ -20,10 +20,11 @@ def create_app():
     db.init_app(app)
     # "route" alamat url yang akan ditangani oleh app ini
     @app.route('/') #decorator(akan memastikan fungsi index akan bisa dipanggil oleh flask)
-    @app.route('/<uri>') #index defined handles without arguments and with argument
-    def index(uri=None):
-        # connect to database use sqlalchemy
-        page = Page.query.filter_by(id=1).first()
+    @app.route('/<url>') #index defined handles without arguments and with argument
+    def index(url=None):
+        if url is not None:
+            page = Page.query.filter_by(url=url).first()             # connect to database use sqlalchemy
+
         contents = 'empty'
         if page is not None:
             contents = page.contents
@@ -48,28 +49,28 @@ def create_app():
         # con.close()
         # return render_template('index.html', TITLE='Flask-01', CONTENT=contents[0])
 
+    #  THIS RUN STATIC
+    # @app.route('/about')
+    # def about():
+    #     return render_template('about.html', TITLE='Flask-01')
 
-    @app.route('/about')
-    def about():
-        return render_template('about.html', TITLE='Flask-01')
-
-    @app.route('/testdb')
-    def testdb():
-    # connect ke database table
-        import psycopg2
-
-    # important "host", here run not in "localhost"
-    # but, this use docker, host run according by name service
-        con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
-    # use object cursor , can be execute syntax database
-        cur = con.cursor()
-
-        cur.execute('select * from page;')
-
-    # cur.fetchone() produce data like the column
-        id, title = cur.fetchone()
-        con.close()
-        return 'output table page: {} - {}'. format(id, title)
+    # @app.route('/testdb')
+    # def testdb():
+    # # connect ke database table
+    #     import psycopg2
+    #
+    # # important "host", here run not in "localhost"
+    # # but, this use docker, host run according by name service
+    #     con = psycopg2.connect('dbname=flask01 user=devuser password=devpassword host=postgres')
+    # # use object cursor , can be execute syntax database
+    #     cur = con.cursor()
+    #
+    #     cur.execute('select * from page;')
+    #
+    # # cur.fetchone() produce data like the column
+    #     id, title = cur.fetchone()
+    #     con.close()
+    #     return 'output table page: {} - {}'. format(id, title)
 
     return app
 
