dab4862693bea97dbe6915d0110d672ffa18b08f
RydhaValenda <ryval.study@gmail.com>
2020-04-30 19:31:53 +0700

    Update - 09 Secure Admin - create user as admin - check pgadmin in table user (you have user root)

--- web_app/scripts/initializedb.py
+++ web_app/scripts/initializedb.py
@@ -6,11 +6,24 @@ import sys,os
 sys.path.append(os.getcwd() + '/web_app') #a ccording mark directory as sources
 
 from app import create_app
-from models import Page, db
+from models import Page, db, Role, User
 
 app = create_app()
 
 with app.app_context():
+    # to add user as admin
+    admin_role = Role()
+    admin_role.name = 'admin'
+    db.session.add(admin_role)
+    db.session.commit()
+
+    root = User()
+    root.email = 'ryval.study@gmail.com'
+    root.password = '123456'
+    root.active = True
+    root.roles.append(admin_role)
+    db.session.add(root)
+
     page = Page()
     page.title = 'Homepage'
     page.contents = "<h1>Welcome to my website</h1>"
